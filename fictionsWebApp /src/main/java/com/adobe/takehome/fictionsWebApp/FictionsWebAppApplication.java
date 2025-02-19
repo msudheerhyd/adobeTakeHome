c2326814ab2f44ccd5aadb874e92be0fdcdccf83
Sudheer Mocharla <msudheerhyd@gmail.com>
2025-02-19 11:37:13 -0800

    Added fictions web app

---
+++ fictionsWebApp /src/main/java/com/adobe/takehome/fictionsWebApp/FictionsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.adobe.takehome.fictionsWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictionsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FictionsWebAppApplication.class, args);
+	}
+
+}
---
+++ fictionsWebApp /src/test/java/com/adobe/takehome/fictionsWebApp/FictionsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adobe.takehome.fictionsWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FictionsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
